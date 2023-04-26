162831544b19df4f1b45818becb8e9f76ca32a3e
brunomont <brunoadrian.mont@gmail.com>
2023-04-26 01:17:26 -0500

    feat: with gpt3.5 is too much text

--- src/services/openai/chat.ts
+++ src/services/openai/chat.ts
@@ -1,15 +1,29 @@
+import handleEscapeChars from "../../utils/handleEscapeChars";
 import { openai } from "./config";
+import fs from "fs";
+const getTextFromFile = (path: string) => {
+	return fs.readFileSync(path, "utf8");
+};
 
 export const chat = async (message: string) => {
-	const { data, statusText } = await openai.createChatCompletion({
+	const informationData = getTextFromFile("text.txt");
+	const { data } = await openai.createChatCompletion({
 		model: "gpt-3.5-turbo",
 		messages: [
+			{
+				content: informationData,
+				role: "system",
+			},
+			{
+				content:
+					"Tú eres el asistente virtual de cada una de las escuelas académicas de la universidad nacional de trujillo. Te encargas de responder las dudas o inquietudes de los estudiantes acerca de cosas de la universidad. Limita tus respuestas con solo la información que se te ha sido proporcionada.",
+				role: "system",
+			},
 			{
 				content: message,
 				role: "user",
 			},
 		],
 	});
-	console.log(statusText);
-	return data.choices[0].message;
+	return handleEscapeChars(data.choices[0].message?.content || "");
 };
