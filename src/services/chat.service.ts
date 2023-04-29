e6e1fc6f82b6923c15667ff41e8c861b8ade0225
brunomont <brunoadrian.mont@gmail.com>
2023-04-28 23:38:46 -0500

    feat: begin with chat service

---
+++ src/services/chat.service.ts
@@ -0,0 +1,28 @@
+import { AddMessageDTO } from "../dto/chat/add-message.dto";
+import { createChatDTO } from "../dto/chat/create-chat.dto";
+import { ChatModel } from "../models";
+
+const createChat = async (chat: createChatDTO) => {
+	const newChat = new ChatModel(chat);
+	await newChat.save();
+	return newChat;
+};
+
+const findChatByPhone = async (phone: string) => {
+	const chat = await ChatModel.findOne({ phone });
+	return chat;
+};
+
+const addMessageToChat = async (phone: string, message: AddMessageDTO) => {
+	const chat = await ChatModel.findOne({ phone });
+	if (!chat) return null;
+	chat.messages.push(message);
+	await chat.save();
+	return chat;
+};
+
+export default {
+	createChat,
+	findChatByPhone,
+	addMessageToChat,
+};
