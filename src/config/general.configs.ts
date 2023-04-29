4473abe044e244bc38b58d1a57c84a9c6aa75d00
brunomont <brunoadrian.mont@gmail.com>
2023-04-29 12:14:51 -0500

    mod: add wtsp stuff to config file

--- src/config/general.configs.ts
+++ src/config/general.configs.ts
@@ -9,9 +9,17 @@ type TConfig = {
 		openai: {
 			apiKey: string;
 		};
+		whatsapp: {
+			appId: string;
+			appSecret: string;
+			recipientWaid: string;
+			version: string;
+			phoneNumberId: string;
+			accessToken: string;
+
+		}
 	};
 };
-
 const config: TConfig = {
 	// General
 	api: {
@@ -25,6 +33,14 @@ const config: TConfig = {
 		openai: {
 			apiKey: process.env.OPENAI_API_KEY || "",
 		},
+		whatsapp: {
+			appId: process.env.WAB_APP_ID || "",
+			appSecret: process.env.WAB_APP_SECRET || "",
+			recipientWaid: process.env.WAB_RECIPIENT_WAID || "",
+			version: process.env.WAB_VERSION || "",
+			phoneNumberId: process.env.WAB_PHONE_NUMBER_ID || "",
+			accessToken: process.env.WAB_ACCESS_TOKEN || "",
+		}
 	},
 };
 
