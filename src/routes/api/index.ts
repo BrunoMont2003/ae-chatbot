c4f71e9416c26114e18787ed780aaad521fe3c33
brunomont <brunoadrian.mont@gmail.com>
2023-05-06 12:26:02 -0500

    add: school route

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,4 +1,5 @@
 import ChatRoutes from "./chat.routes"
+import SchoolRoutes from "./school.routes";
 import WebhookRoutes from "./webhook.routes";
 
 import { Router } from "express";
@@ -6,6 +7,7 @@ import { Router } from "express";
 const router = Router();
 
 router.use(ChatRoutes);
+router.use(SchoolRoutes);
 router.use(WebhookRoutes);
 
 export { router };
