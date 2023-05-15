2444dadc548b8a572289c8e3779574fd3af0d96c
brunomont <brunoadrian.mont@gmail.com>
2023-05-15 18:28:54 -0500

    drop adminuser

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,4 @@
-import AdminUserModel from "./admin-user/admin-user.model";
 import ChatModel from "./chat/chat.model";
 import SchoolModel from "./school/school.model";
 
-export { AdminUserModel, ChatModel, SchoolModel };
+export { ChatModel, SchoolModel };
