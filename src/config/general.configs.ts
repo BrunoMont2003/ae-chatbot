7853763b9daefab7481641ec3de0b76a0f8903c1
brunomont <brunoadrian.mont@gmail.com>
2023-05-15 22:30:37 -0500

    add: default connection string local

--- src/config/general.configs.ts
+++ src/config/general.configs.ts
@@ -30,7 +30,7 @@ const config: TConfig = {
 	},
 	// Database
 	mongo: {
-		uri: process.env.MONGO_URI || "mongodb://localhost:27017",
+		uri: process.env.MONGO_URI || "mongodb://127.0.0.1:27017",
 	},
 	external: {
 		openai: {
