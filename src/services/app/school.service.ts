9e0e51cd686097b8913e07a7249b99e8d42f7d0a
brunomont <brunoadrian.mont@gmail.com>
2023-05-17 15:39:29 -0500

    drop: faqs selection in get school

--- src/services/app/school.service.ts
+++ src/services/app/school.service.ts
@@ -3,7 +3,7 @@ import { SchoolModel } from "../../models";
 const getSchoolBySlug = async ({ slug }: { slug: string }) => {
     const school = await SchoolModel.findOne({
         slug
-    }).select("-__v -_id -periods._id -periods.cycles._id -periods.cycles.subjects._id -periods.cycles.subjects.professor._id -slug -faqs._id");
+    }).select("-__v -_id -periods._id -periods.cycles._id -periods.cycles.subjects._id -periods.cycles.subjects.professor._id -slug -faqs");
     return school;
 }
 const getSchoolFaqs = async ({ slug }: { slug: string }) => {
